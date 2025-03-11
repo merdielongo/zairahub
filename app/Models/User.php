cf59e9b828e56134e1d68cdcd9e4f60221615765
merdielongo <merdielongo9@gmail.com>
2025-03-11 13:34:32 +0100

    Feat: integration de spatie permission et ajout de deleted_at dans la table users

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,11 +9,13 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasPermissions;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, SoftDeletes;
+    use HasFactory, Notifiable, SoftDeletes, HasRoles, HasPermissions;
 
     /**
      * The attributes that are mass assignable.
