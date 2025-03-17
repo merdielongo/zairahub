8c5bde04bb5ca2dc43076f509d0d222746d5d9ee
merdielongo <merdielongo9@gmail.com>
2025-03-17 14:25:59 +0100

    Fix: suppression de l'importation inutile de usePage dans AppSidebar.vue

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -4,7 +4,7 @@ import NavMain from '@/components/NavMain.vue';
 import NavUser from '@/components/NavUser.vue';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
-import { Link, usePage } from '@inertiajs/vue3';
+import { Link } from '@inertiajs/vue3';
 import { LayoutGrid, Home, Building, CreditCard, ReplaceAll, Slack } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
 
